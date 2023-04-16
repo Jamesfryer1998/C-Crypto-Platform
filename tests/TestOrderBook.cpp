3017089e7cdb5bb041b041bd1365b465e0318cbb
Andy Schneider <andy@schnoberts.com>
2023-04-16 18:21:17 +0100

    Add testStats in GTest form

---
+++ tests/TestOrderBook.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include <crypto-platform/OrderBook.h>
+
+TEST(OrderBookTest, TestStats)
+{
+    double highPrice = 3;
+    double lowPrice = 1;
+    double avgPrice = 2;
+    double spread = 2;
+    OrderBook::OrderBookEntryList orders;
+    OrderBookEntry entry1("2020/03/17 17:01:24.884492","ETH/BTC",OrderBookType::bid,1,1);
+    OrderBookEntry entry2("2020/03/17 17:01:24.884492","ETH/BTC",OrderBookType::bid,2,1);
+    OrderBookEntry entry3("2020/03/17 17:01:24.884492","ETH/BTC",OrderBookType::bid,3,1);
+    orders.push_back(entry1);
+    orders.push_back(entry2);
+    orders.push_back(entry3);
+
+    std::cout << "\nTests:" << std::endl;
+
+    EXPECT_EQ(OrderBook::getHighPrice(orders), highPrice);
+    EXPECT_EQ(OrderBook::getLowPrice(orders), lowPrice);
+    EXPECT_EQ(OrderBook::getHighPrice(orders) - OrderBook::getLowPrice(orders), spread);
+    EXPECT_EQ(OrderBook::getAvgPrice(orders), avgPrice);
+}
